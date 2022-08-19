052d63e16bcb8cb0c8ef3e770e071ba752ee7ef3
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2022-08-18 21:30:26 -0400

    .

--- src/utils/omit.ts
+++ src/utils/omit.ts
@@ -4,7 +4,8 @@ function entries<T extends object>(object: T) {
 
 export default function omit<T extends object, U extends [...(keyof T)[]]>(object: T, ...keys: [...U]) {
 	const newObject = {} as Omit<T, U[number]>,
-	 objectEntries = entries(object).filter(([k]) => !keys.includes(k)) as [Exclude<keyof T, U[number]>, T[Exclude<keyof T, U[number]>]][];
+	 objectEntries = entries(object)
+	 	.filter(([k]) => !keys.includes(k)) as [Exclude<keyof T, U[number]>, T[Exclude<keyof T, U[number]>]][];
 
 	for (const [k, v] of objectEntries)
 		newObject[k] = v;
