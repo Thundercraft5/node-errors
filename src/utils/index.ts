79531ad4bdc29955c3df779f0340798f0e732fc2
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2022-09-12 13:54:28 -0400

    .

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,3 @@ export { default as indent } from "./indent";
 export { default as isCodedError } from "./isCodedError";
 export { default as isCodedErrorClass } from "./isCodedErrorClass";
 export { default as omit } from "./omit";
-
-export class Test {}
